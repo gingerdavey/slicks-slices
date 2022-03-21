4b4cb3c3ebb0f0f08b132ca99090b240ba6e1fb9
Robbie Davey <robbie@limely.co.uk>
2022-03-21 18:12:30 +0000

    Update 2

--- gatsby/src/components/OrderContext.js
+++ gatsby/src/components/OrderContext.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 
+// Create a order context
 const OrderContext = React.createContext();
 
-export const OrderProvider = ({ children }) => {
+export function OrderProvider({ children }) {
+  // we need to stick state in here
   const [order, setOrder] = useState([]);
-
   return (
     <OrderContext.Provider value={[order, setOrder]}>
       {children}
     </OrderContext.Provider>
   );
-};
+}
 
 export default OrderContext;
